a1054240c808e73f79561a7d2a27812627e7ace3
N6HAN <spica.han@gmail.com>
2025-10-20 06:54:20 -0700

    Upgrade Project and FW by CubeMX

--- Inc/stm32f7xx_it.h
+++ Inc/stm32f7xx_it.h
@@ -1,62 +1,60 @@
+/* USER CODE BEGIN Header */
 /**
   ******************************************************************************
-  * @file    USB_Host/HID_Standalone/Inc/stm32f7xx_it.h 
-  * @author  MCD Application Team
-  * @version V1.0.2
-  * @date    18-November-2015 
+  * @file    stm32f7xx_it.h
   * @brief   This file contains the headers of the interrupt handlers.
   ******************************************************************************
   * @attention
   *
-  * <h2><center>&copy; COPYRIGHT(c) 2015 STMicroelectronics</center></h2>
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
   *
-  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
-  * You may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at:
-  *
-  *        http://www.st.com/software_license_agreement_liberty_v2
-  *
-  * Unless required by applicable law or agreed to in writing, software 
-  * distributed under the License is distributed on an "AS IS" BASIS, 
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
   *
   ******************************************************************************
   */
+/* USER CODE END Header */
 
 /* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef __STM32F7xx_IT_H
 #define __STM32F7xx_IT_H
 
 #ifdef __cplusplus
- extern "C" {
-#endif 
+extern "C" {
+#endif
 
-/* Includes ------------------------------------------------------------------*/
-#include "main.h"
-//#define    USE_USB_FS
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
 /* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
 /* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
 /* Exported macro ------------------------------------------------------------*/
-/* Exported functions ------------------------------------------------------- */
-void NMI_Handler(void);
-void HardFault_Handler(void);
-void MemManage_Handler(void);
-void BusFault_Handler(void);
-void UsageFault_Handler(void);
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
 void SVC_Handler(void);
-void DebugMon_Handler(void);
 void PendSV_Handler(void);
 void SysTick_Handler(void);
+/* USER CODE BEGIN EFP */
 
-void AUDIO_IN_SAIx_DMAx_IRQHandler(void);
-void AUDIO_OUT_SAIx_DMAx_IRQHandler(void);
+/* USER CODE END EFP */
 
 #ifdef __cplusplus
 }
 #endif
 
 #endif /* __STM32F7xx_IT_H */
-
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
--- Src/stm32f7xx_it.c
+++ Src/stm32f7xx_it.c
@@ -1,163 +1,115 @@
+/* USER CODE BEGIN Header */
 /**
- ******************************************************************************
- * @file    USB_Host/HID_Standalone/Src/stm32f7xx_it.c
- * @author  MCD Application Team
- * @version V1.0.2
- * @date    18-November-2015
- * @brief   Main Interrupt Service Routines.
- *          This file provides template for all exceptions handler and
- *          peripherals interrupt service routine.
- ******************************************************************************
- * @attention
- *
- * <h2><center>&copy; COPYRIGHT(c) 2015 STMicroelectronics</center></h2>
- *
- * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
- * You may not use this file except in compliance with the License.
- * You may obtain a copy of the License at:
- *
- *        http://www.st.com/software_license_agreement_liberty_v2
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- ******************************************************************************
- */
+  ******************************************************************************
+  * @file    stm32f7xx_it.c
+  * @brief   Interrupt Service Routines.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
 
 /* Includes ------------------------------------------------------------------*/
+#include "main.h"
 #include "stm32f7xx_it.h"
-#include "stm32746g_discovery_audio.h"
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+/* USER CODE END Includes */
+
 /* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN TD */
+
+/* USER CODE END TD */
+
 /* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
 /* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
 /* Private variables ---------------------------------------------------------*/
+/* USER CODE BEGIN PV */
 
-/* SAI handler declared in "stm32746g_discovery_audio.c" file */
-extern SAI_HandleTypeDef haudio_out_sai;
-/* I2S handler declared in "stm32746g_discovery_audio.c" file */
-extern SAI_HandleTypeDef haudio_in_sai;
-
-//extern UART_HandleTypeDef UartHandle;
+/* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
-/* Private functions ---------------------------------------------------------*/
-//#define    USE_USB_FS
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/* External variables --------------------------------------------------------*/
+
+/* USER CODE BEGIN EV */
+
+/* USER CODE END EV */
+
 /******************************************************************************/
-/*            Cortex-M7 Processor Exceptions Handlers                         */
+/*           Cortex-M7 Processor Interruption and Exception Handlers          */
 /******************************************************************************/
-
 /**
- * @brief   This function handles NMI exception.
- * @param  None
- * @retval None
- */
-void NMI_Handler(void) {
+  * @brief This function handles System service call via SWI instruction.
+  */
+void SVC_Handler(void)
+{
+  /* USER CODE BEGIN SVCall_IRQn 0 */
+
+  /* USER CODE END SVCall_IRQn 0 */
+  /* USER CODE BEGIN SVCall_IRQn 1 */
+
+  /* USER CODE END SVCall_IRQn 1 */
 }
 
 /**
- * @brief  This function handles Hard Fault exception.
- * @param  None
- * @retval None
- */
-void HardFault_Handler(void) {
-	/* Go to infinite loop when Hard Fault exception occurs */
-	while (1) {
-	}
+  * @brief This function handles Pendable request for system service.
+  */
+void PendSV_Handler(void)
+{
+  /* USER CODE BEGIN PendSV_IRQn 0 */
+
+  /* USER CODE END PendSV_IRQn 0 */
+  /* USER CODE BEGIN PendSV_IRQn 1 */
+
+  /* USER CODE END PendSV_IRQn 1 */
 }
 
 /**
- * @brief  This function handles Memory Manage exception.
- * @param  None
- * @retval None
- */
-void MemManage_Handler(void) {
-	/* Go to infinite loop when Memory Manage exception occurs */
-	while (1) {
-	}
-}
+  * @brief This function handles System tick timer.
+  */
+void SysTick_Handler(void)
+{
+  /* USER CODE BEGIN SysTick_IRQn 0 */
 
-/**
- * @brief  This function handles Bus Fault exception.
- * @param  None
- * @retval None
- */
-void BusFault_Handler(void) {
-	/* Go to infinite loop when Bus Fault exception occurs */
-	while (1) {
-	}
-}
+  /* USER CODE END SysTick_IRQn 0 */
+  HAL_IncTick();
+  /* USER CODE BEGIN SysTick_IRQn 1 */
 
-/**
- * @brief  This function handles Usage Fault exception.
- * @param  None
- * @retval None
- */
-void UsageFault_Handler(void) {
-	/* Go to infinite loop when Usage Fault exception occurs */
-	while (1) {
-	}
-}
-
-/**
- * @brief  This function handles SVCall exception.
- * @param  None
- * @retval None
- */
-void SVC_Handler(void) {
-}
-
-/**
- * @brief  This function handles Debug Monitor exception.
- * @param  None
- * @retval None
- */
-void DebugMon_Handler(void) {
-}
-
-/**
- * @brief  This function handles PendSVC exception.
- * @param  None
- * @retval None
- */
-void PendSV_Handler(void) {
-}
-
-/**
- * @brief  This function handles SysTick Handler.
- * @param  None
- * @retval None
- */
-void SysTick_Handler(void) {
-	HAL_IncTick();
-	// Toggle_Leds();
+  /* USER CODE END SysTick_IRQn 1 */
 }
 
 /******************************************************************************/
-/*                 STM32F7xx Peripherals Interrupt Handlers                   */
-/*  Add here the Interrupt Handler for the used peripheral(s) (PPP), for the  */
-/*  available peripheral interrupt handler's name please refer to the startup */
-/*  file (startup_stm32f7xx.s).                                               */
+/* STM32F7xx Peripheral Interrupt Handlers                                    */
+/* Add here the Interrupt Handlers for the used peripherals.                  */
+/* For the available peripheral interrupt handler names,                      */
+/* please refer to the startup file (startup_stm32f7xx.s).                    */
 /******************************************************************************/
 
-/**
- * @brief  This function handles USB-On-The-Go FS/HS global interrupt request.
- * @param  None
- * @retval None
- */
+/* USER CODE BEGIN 1 */
 
-/**
- * @brief This function handles DMA2 Stream 4 interrupt request.
- * @param None
- * @retval None
- */
-void AUDIO_OUT_SAIx_DMAx_IRQHandler(void) {
-	HAL_DMA_IRQHandler(haudio_out_sai.hdmatx);
-}
-
-void AUDIO_IN_SAIx_DMAx_IRQHandler(void) {
-	HAL_DMA_IRQHandler(haudio_in_sai.hdmarx);
-}
-
+/* USER CODE END 1 */
